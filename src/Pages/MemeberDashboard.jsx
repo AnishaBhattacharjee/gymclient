edcaaf976de93559220ae3c74f591c43faf68c7b
AnishaBhattacharjee <anishab163@gmail.com>
2024-01-02 20:26:04 +0530

    dashboard

--- src/Pages/MemeberDashboard.jsx
+++ src/Pages/MemeberDashboard.jsx
@@ -23,11 +23,11 @@ const MemeberDashboard = () => {
     <>
       <div className="container-fluid page-header mb-5">
         <div className="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style={{ minHeight: 400 }}>
-          <h4 className="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase font-weight-bold">About Us</h4>
+          <h4 className="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase font-weight-bold">Member Dashboard</h4>
           <div className="d-inline-flex">
             <p className="m-0 text-white"><a className="text-white" href>Home</a></p>
             <p className="m-0 text-white px-2">/</p>
-            <p className="m-0 text-white">About Us</p>
+            <p className="m-0 text-white">Dashboard</p>
           </div>
         </div>
       </div>
